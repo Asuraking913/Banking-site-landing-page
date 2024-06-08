25fde4deb01dc239bd003192beb6dd7d1ecd9a05
Israel Shedrack <israelshedrack913@gmail.com>
2024-06-07 21:05:14 -0400

    Finished responsiveness

--- src/components/bodyHead.jsx
+++ src/components/bodyHead.jsx
@@ -127,22 +127,22 @@ function Body1() {
 
   return (
     <article>
-        <section className='h-screen media1 flex p-[2em] sm:p-0 sm:pl-[4em]'>
-            <a href="" className='poppins flex items-center justify-center gap-[0.5em] font-bold capitalize absolute p-[0.7em] sm:p-[1em] px-[1.5em] boxSha rounded-[2em] top-[83%] right-[7%] sm:right-[42%] sm:top-[20%]'><i className='fa-solid fa-car-side text-pink-600 text-2xl'></i>Car loans</a>
-            <a href="" className='poppins flex items-center justify-center gap-[0.5em] font-bold capitalize absolute p-[0.7em] sm:p-[1em] px-[1.5em] boxSha rounded-[2em] right-[7%] top-[22%] sm:right-[3%] sm:top-[43%]'><i className='fa-solid fa-house text-pink-600 text-2xl'></i>Home loans</a>
+        <section className='h-screen w-[100%] flex p-[2em] sm:p-0 sm:pl-[4em] screen1'>
+            <a href="" className='poppins flex items-center justify-center gap-[0.5em] font-bold capitalize absolute p-[0.7em] top1 sm:p-[1em] px-[1.5em] boxSha rounded-[2em] top-[83%] right-[7%] sm:right-[42%] sm:top-[20%]'><i className='fa-solid fa-car-side text-pink-600 text-2xl'></i>Car loans</a>
+            <a href="" className='poppins flex items-center justify-center gap-[0.5em] font-bold capitalize absolute p-[0.7em] sm:p-[1em] px-[1.5em] boxSha rounded-[2em] right-[1em] top-[105px] sm:right-[3%] sm:top-[43%]'><i className='fa-solid fa-house text-pink-600 text-2xl'></i>Home loans</a>
             <div className='w-full h-[100%]  sm:mt-0 mt-[6em] gap-[2.5em] sm:gap-[1.5em] flex flex-col sm:items-start sm:justify-center'>
                 <h1 className='sm:text-[3rem] text-[2rem] line poppins font-extrabold capitalize'>Chase your <span className='block'>dream with us</span></h1>
                 <p className='text-xl'>The harder you work for something, the greater
                     <span className='block'>you'll feel when you achieve it.</span>
                 </p>
                 <p className='flex gap-4'>
-                    <a href="" className='py-[1em] px-[1.5em] bg-pink-600 font-semibold text-white rounded-[5px] uppercase'>apply online</a>
-                    <a href="" className='py-[1em] px-[1.5em] border-2 border-pink-600 text-pink-600 uppercase font-semibold rounded-[5px]'>Loan calculator</a>
+                    <a href="" className='py-[1em] px-[1.5em] pad1 bg-pink-600 font-semibold text-white rounded-[5px] uppercase'>apply online</a>
+                    <a href="" className='py-[1em] px-[1.5em] pad1 border-2 border-pink-600 text-pink-600 uppercase font-semibold rounded-[5px]'>Loan calculator</a>
                 </p>
-                <div className='flex gap-[1em] items-center justify-center absolute bottom-[10%] sm:bottom-0'>
-                    <i className='fa-solid fa-phone text-pink-600 text-[1.2rem] text6 '></i>
-                    <p className='poppins font-semibold'>+234</p>
-                    <p className='poppins font-bold'>|</p>
+                <div className='sm:flex gap-[1em] items-center bottom1 justify-center absolute bottom-[10%] sm:bottom-0'>
+                    <i className='fa-solid fa-phone sm:block inline text-pink-600 text-[1.2rem] text6 '></i>
+                    <p className='poppins font-semibold sm:block inline'>+234</p>
+                    <p className='poppins font-bold sm:block inline'>|</p>
                     <div className='flex justify-center items-center gap-[0.5em]'>
                         <i className='fa-brands hover:scale-125 text-2xl duration-[0.5s] fa-facebook text-blue-500'></i>
                         <i className='fa-brands hover:scale-125 text-2xl duration-[0.5s] fa-youtube text-red-600'></i>
@@ -155,7 +155,7 @@ function Body1() {
                 <img src={myImage} className='absolute bottom-0 right-[10em]' alt="" />
             </div>
         </section>
-        <section className='sm:h-screen p-[1em] sm:p-[3rem] py-[1em] sm:py-[6rem] flex flex-col items-center justify-center'>
+        <section className='sm:h-screen screen21 p-[1em] sm:p-[3rem] py-[1em] sm:py-[6rem] flex flex-col items-center justify-center'>
             <div className='w-[100%] text-center flex items-center flex-col gap-[1em]'>
                 <h2 className='sm:text-4xl text-2xl poppins font-bold capitalize '>Creating extraordinary customer experience</h2>
                 <h2 className='w-[80%] text-gray-500 sm:pb-[2em] text-xl text-[1.1em]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga fugiat dolorum error doloremque praesentium itaque! Lorem ipsum dolor sit Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -166,7 +166,7 @@ function Body1() {
             </div>
             
         </section>
-        <section className='flex justify-center items-center flex-col sm:h-auto h-[80vh] gap-[0em] sm:gap-[2.5em]'>
+        <section className='flex justify-center screen21 items-center flex-col sm:h-auto h-[80vh] gap-[0em] sm:gap-[2.5em]'>
             <h2 className='poppins text-3xl font-bold pt-[6em]'>Buisness Partners</h2>
             <div className='sm:flex grid grid-cols-2 grid-rows-2 gap-[1em]  sm:gap-[10em]'>
                 <div className='w-[200px] h-[200px]'>
@@ -183,7 +183,7 @@ function Body1() {
                 </div>
             </div>
         </section>
-        <section className='flex sm:h-screen h-[80vh] w-full items-center gap-[4em]'>
+        <section className='flex sm:h-screen h-[80vh] screen3 w-full items-center gap-[4em]'>
             <div className='w-[500px] sm:flex hidden bg-cover h-[65%]  bgImg justify-center items-center'></div>
             <div className='w-[600px] p-[2em] flex flex-col gap-[1em]'>
                 <h2 className='text-2xl poppins font-bold'>IPDC AT A GLANCE</h2>
@@ -192,19 +192,19 @@ function Body1() {
                 <a href="" className='text-pink-600 poppins font-semibold'>Read More <i className='fa-solid fa-arrow-right'></i></a>
             </div>
         </section>
-        <section className='sm:p-[3rem] px-[2em] flex flex-col justify-center items-center gap-[1.5em]'>
+        <section className='sm:p-[3rem] screen1 px-[2em] flex flex-col justify-center items-center gap-[1.5em]'>
             <h2 className='poppins text-3xl capitalize font-bold'>Our services</h2>
             <p className='text-gray-600 text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam, non! Lorem, ipsum dolor.</p>
             <div className='sm:flex grid grid-cols-2 grid-rows-2 h-[50%] sm:flex-row  w-[100%] gap-[2em] p-[1em]'>{serverList}</div>
         </section>
-        <section className='h-[100vh] flex flex-col items-center justify-center py-[2em] px-[1em] sm:p-[3rem] gap-[2em]'>
+        <section className='h-[100vh] screen4 flex flex-col items-center justify-center py-[2em] px-[1em] sm:p-[3rem] gap-[2em]'>
             <div className='flex flex-col justify-center items-center  gap-[2em]'>
                 <h2 className='capitalize text-center text-3xl font-bold poppins'>Our best results for the year</h2>
                 <p className='text-gray-600 text-center w-[80%]'>Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed. adipisicing elit. Quaerat officia delectus, eum nihil aliquam non.</p>
             </div>
                 <div className='w-[90%] grid grid-cols-3 grid-rows-3 sm:grid-cols-4 sm:grid-rows-2 gap-[2em] sm:gap-[5em] place-items-center'>{resultList}</div>
         </section>
-        <section className='p-[4rem] flex flex-col justify-center items-center h-[50vh]'>
+        <section className='p-[4rem] flex flex-col screen5 justify-center items-center h-[50vh]'>
             <News />
         </section>
         <Foot />
